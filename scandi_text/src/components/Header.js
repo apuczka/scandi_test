017c6d0a785a08eae9dc69c6e2f1081df0b51b1a
apuczka <a.puczka@icloud.com>
2022-11-08 22:11:41 +0100

    header

---
+++ scandi_text/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from '../assets/Group.png';
+import currency from '../assets/currency.png';
+import emptyCart from '../assets/EmptyCart.png';
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='navbar'
+      >
+        <nav>WOMEN</nav>
+        <nav>MEN</nav>
+        <nav>KIDS</nav>
+
+        <img src={logo} alt='logo' />
+        <img src={currency} alt="currency" />
+        <img src={emptyCart} alt="empty cart" />
+
+
+      </div>
+    )
+  }
+}
